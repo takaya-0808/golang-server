370071340de6eed4ffc3942971037b75c86d285b
宮城孝明 <takaya1912@gmail.com>
2020-02-27 18:14:02 +0900

    Add function  Create tokenID and Response tokenID

--- src/server/gin/main.go
+++ src/server/gin/main.go
@@ -3,6 +3,7 @@ package main
 import(
 	"net/http"	
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type UserInfo struct {
@@ -11,20 +12,33 @@ type UserInfo struct {
 	Password  string   `json:"password"`
 }
 
+type TokenResponse struct {
+	AccessToken string  `json:"accesstoken"`
+}
+
 func main() {
 	router := gin.Default()
-
 	router.POST("/",Response)
 	router.Run(":8800")
 }
 
 func Response(ctx *gin.Context) {
 
-	userinfo := UserInfo{
-		Username: "hoge",
-		Email:    "1222@gmail.com",
-		Password: "password",
+	var userinfo UserInfo
+	ctx.BindJSON(&userinfo)
+	token := CreatToken()
+	tokenrequest := TokenResponse{
+		AccessToken: *token,
 	}
+	ctx.JSON(http.StatusOK,tokenrequest)
+}
 
-	ctx.JSON(http.StatusOK,userinfo)
+func CreatToken() *string {
+	u, err := uuid.NewRandom()
+	if err != nil {
+		panic(err)
+		return nil
+	}
+	uu := u.String()
+	return &uu
 }
